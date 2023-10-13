4b5c0b90a8d21737e9cb1d9b31f5c3abd7385000
IskanderSabirov <sabiroviskander@yandex.ru>
2023-10-14 02:55:35 +0300

    analyzer: added structs for analyzer and summarizer

--- src/analyzer/analyzer.go
+++ src/analyzer/analyzer.go
@@ -1,6 +1,10 @@
 package main
 
-func analyze(topics map[string]struct{}, message string) map[string]struct{} {
-	//TODO()
-	return nil
+type Analyzer struct {
+	summarizer Summarizer
+}
+
+func (a *Analyzer) analyze(topics map[string]struct{}, message string) (map[string]struct{}, string, error) {
+	//TODO() нужно будет возвращать подходящие топики, summary сообщения и ошибку
+	panic("implement me")
 }
