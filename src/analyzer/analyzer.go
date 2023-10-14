d41b1dc127ec004e56d5dd6dee18daf180da156d
IskanderSabirov <sabiroviskander@yandex.ru>
2023-10-14 19:58:39 +0300

    analyzer: updated localDB

--- src/analyzer/analyzer.go
+++ src/analyzer/analyzer.go
@@ -1,10 +1,17 @@
 package main
 
-type Analyzer struct {
-	summarizer Summarizer
+type BasicTextAnalyzer interface {
+	contains(text string, keyWord string) float64
 }
 
-func (a *Analyzer) analyze(topics map[string]struct{}, message string) (map[string]struct{}, string, error) {
-	//TODO() нужно будет возвращать подходящие топики, summary сообщения и ошибку
+type Analyzer struct{}
+
+func (a Analyzer) contains(text string, keyWord string) float64 {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a Analyzer) analyze(topics map[string]struct{}, message string) ([]string, error) {
+	//TODO() нужно будет возвращать подходящие топики и ошибку
 	panic("implement me")
 }
--- src/analyzer/summarizer.go
+++ src/analyzer/summarizer.go
@@ -1,14 +1,18 @@
 package main
 
+import "math"
+
 type Summarizer interface {
-	Summarize(text string) (string, error)
+	summarize(text string) (string, error)
 }
 
 type MessageSummarizer struct {
 	textLength int
 }
 
-func (m MessageSummarizer) Summarize(text string) (string, error) {
-	length := min(m.textLength, len(text))
-	return text[:length-1], nil
+func (m MessageSummarizer) summarize(text string) (string, error) {
+
+	length := math.Min(float64(m.textLength), float64(len(text)))
+
+	return text[:int(length)-1], nil
 }
