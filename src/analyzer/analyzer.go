56c3faea826cfac92e44730a37b88fa10887a3c1
IskanderSabirov <sabiroviskander@yandex.ru>
2023-10-15 23:03:12 +0300

    analyzer: Аня Лихтар added analyzer function and tests

--- src/analyzer/analyzer.go
+++ src/analyzer/analyzer.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 type BasicTextAnalyzer interface {
 	contains(text string, keyWord string) float64
 }
@@ -7,8 +9,12 @@ type BasicTextAnalyzer interface {
 type Analyzer struct{}
 
 func (a Analyzer) contains(text string, keyWord string) float64 {
-	//TODO implement me
-	panic("implement me")
+	text = strings.ToLower(text)
+	keyWord = strings.ToLower(keyWord)
+	if strings.Contains(text, keyWord) {
+		return 1.0
+	}
+	return 0.0
 }
 
 func (a Analyzer) analyze(topics map[string]struct{}, message string) ([]string, error) {
