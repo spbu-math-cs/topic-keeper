71b51c0f92e5cf5239b70c1c0192c972291324ca
KAlina28 <krylovaalina28@yandex.ru>
2023-10-29 23:21:04 +0300

    hints

--- src/flow/main.go
+++ src/flow/main.go
@@ -1,25 +1,171 @@
 package main
 
 import (
-	"encoding/json"
+	_ "embed"
+	"errors"
+	"fmt"
+	"hash/fnv"
 	"log"
 	"os"
 	"strings"
 
+	"gopkg.in/yaml.v2"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-type ChannelData struct {
-	Command string `json:"command"`
-	Name    string `json:"name"`
-	Topic   string `json:"topic"`
+var wrongFmtError = errors.New("Неправильный формат команды")
+
+const (
+	NWorkers      = 30
+	BaseCap       = 20
+	summaryLength = 50
+)
+
+//go:embed db_config.yml
+var rawDBConfig []byte
+
+type workEvent struct {
+	channelName string
+	text        string
+	ID          int
+}
+
+type sendEvent struct {
+	text string
+	user string
+}
+
+var (
+	api       API
+	bot       *tgbotapi.BotAPI
+	dataBase  LocalStorage
+	sendChan  chan sendEvent
+	openAIkey string
+)
+
+func parseTopic(s string) (Concern, error) {
+	s = strings.TrimSpace(s)
+	chanName, topicName, found := strings.Cut(s, " ")
+	if !found {
+		return Concern{}, wrongFmtError
+	}
+	chanName = strings.Trim(chanName, "\n ")
+	if !strings.HasPrefix(chanName, "@") {
+		return Concern{}, wrongFmtError
+	}
+	return Concern{
+		Channel: chanName[1:],
+		Topic:   topicName,
+	}, nil
+}
+
+//echo $TOPIC_KEEPER_TOKEN
+//export TOPIC_KEEPER_TOKEN="6638697091:AAHhpaS-rXlgWXHQzlfa3tAGUoRctKp8n2Q"
+
+//echo $TOPIC_KEEPER_OPENAI_TOKEN
+//export TOPIC_KEEPER_OPENAI_TOKEN=""
+
+const format = `Topic was detected: [%s]
+In channel: @%s
+Summary: %s
+link: https://t.me/%s/%d
+`
+
+func worker(workChan chan workEvent) {
+	for update := range workChan {
+		channel := update.channelName
+		msg := update.text
+
+		if found, err := dataBase.containsChannel(channel); !found || err != nil {
+			if err != nil {
+				log.Printf(err.Error())
+			}
+			continue
+		}
+
+		possibleTopics, err := dataBase.getTopics(channel)
+		if err != nil {
+			log.Printf(err.Error())
+			continue
+		}
+
+		var foundTopics []string
+		if foundTopics, err = api.analyze(msg, possibleTopics); err != nil {
+			log.Printf(err.Error())
+			continue
+		}
+
+		var summary string
+		if openAIkey != "" && len(msg) > summaryLength {
+			if summary, err = api.summarize(msg, openAIkey); err != nil {
+				log.Printf("error in OpenAI uisng with error: %s \n", err.Error())
+				summary = summarize(msg)
+			}
+		} else {
+			summary = summarize(msg)
+		}
+
+		sendUsers, err := dataBase.getUsers(channel, foundTopics)
+		for user, userTopics := range sendUsers {
+			for _, topic := range userTopics {
+				if err := dataBase.setTime(user, channel, topic); err != nil {
+					log.Printf(err.Error())
+					continue
+				}
+			}
+			finalTopics := strings.Join(userTopics, ", ")
+			ans := fmt.Sprintf(format, finalTopics, channel, summary, channel, update.ID)
+
+			sendChan <- sendEvent{
+				text: ans,
+				user: user,
+			}
+		}
+	}
+}
+
+func sender() {
+	for msg := range sendChan {
+		sendMessage(msg.user, msg.text)
+	}
+}
+
+func getHash(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
 }
 
 func main() {
+	workChans := make([]chan workEvent, NWorkers)
+	sendChan = make(chan sendEvent, BaseCap)
+
+	for i := 0; i < NWorkers; i++ {
+		workChans[i] = make(chan workEvent)
+		go worker(workChans[i])
+	}
+	go sender()
+
+	var dbConfig DBConfig
+	var err error
+	if err := yaml.Unmarshal(rawDBConfig, &dbConfig); err != nil {
+		panic(err)
+	}
+	dataBase, err = NewDatabase(dbConfig)
+	if err != nil {
+		panic(err)
+	}
+
 	token := os.Getenv("TOPIC_KEEPER_TOKEN")
-	bot, err := tgbotapi.NewBotAPI(token)
+	openAIkey = os.Getenv("TOPIC_KEEPER_OPENAI_TOKEN")
+
+	bot, err = tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Fatal(err.Error())
+	}
+	if openAIkey != "" {
+		log.Printf("using openAI summarizer with key: %s", openAIkey)
 	}
 
 	bot.Debug = true
@@ -40,90 +186,133 @@ func main() {
 		),
 	)
 
+	api = basicAPI{}
 	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
+		switch {
+		case update.ChannelPost != nil:
+			hsh := getHash(update.ChannelPost.Chat.UserName)
+			workChans[hsh%NWorkers] <- workEvent{
+				channelName: update.ChannelPost.Chat.UserName,
+				text:        update.ChannelPost.Text,
+				ID:          update.ChannelPost.MessageID,
+			}
+		case update.Message != nil:
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Выберите команду:")
+			msg.ReplyMarkup = keyboard
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Выберите команду:")
-		msg.ReplyMarkup = keyboard
-		switch update.Message.Text {
-		case "/start":
-			handleStart(bot, update.Message.Chat.ID)
-		case "/view":
-			handleView(bot, update.Message.Chat.ID)
-		case "/help":
-			handleHelp(bot, update.Message.Chat.ID)
-		default:
-			if strings.HasPrefix(update.Message.Text, "/remove") || strings.HasPrefix(update.Message.Text, "/add") {
-				handleRemoveAdd(bot, update.Message.Chat.ID, update.Message.Text)
-			} else {
-				handleUnknownCommand(bot, update.Message.Chat.ID)
+			updText := strings.Trim(update.Message.Text, "\n ")
+			uname := update.Message.Chat.UserName
+			err := dataBase.addUser(uname, update.Message.Chat.ID)
+			if err != nil {
+				continue
+			}
+
+			switch updText {
+			case "/start":
+				handleStart(uname)
+			case "/view":
+				handleView(uname)
+			case "/help":
+				handleHelp(uname)
+			default:
+				if strings.HasPrefix(updText, "/add") {
+					handleAdd(uname, updText)
+				} else if strings.HasPrefix(updText, "/remove") {
+					handleRemove(uname, updText)
+				} else {
+					handleUnknownCommand(uname)
+				}
 			}
 		}
 	}
 }
 
-func sendMessage(bot *tgbotapi.BotAPI, chatID int64, text string) {
-	msg := tgbotapi.NewMessage(chatID, text)
-	_, err := bot.Send(msg)
+func sendMessage(username string, text string) {
+	userId, err := dataBase.getID(username)
 	if err != nil {
-		log.Panic(err)
+		panic(err.Error())
+	}
+	msg := tgbotapi.NewMessage(userId, text)
+	_, err = bot.Send(msg)
+	if err != nil {
+		log.Println(err.Error())
 	}
 }
 
-func handleStart(bot *tgbotapi.BotAPI, chatID int64) {
+func handleStart(username string) {
 	reply := "Привет! Бот предназначен для помощи в быстром и эффективном поиске нужной информации в чатах и темах на основе предоставленного списка."
-	sendMessage(bot, chatID, reply)
-	handleHelp(bot, chatID)
+	sendMessage(username, reply)
+	handleHelp(username)
 }
 
-func handleView(bot *tgbotapi.BotAPI, chatID int64) {
-	//как-то взять json распарсить и вывести корректно
-	reply := "Список каналов с выранными темами: \n Канал: Как стать миллионером \n Темы: новичок"
-	sendMessage(bot, chatID, reply)
-}
-
-func handleRemoveAdd(bot *tgbotapi.BotAPI, chatID int64, text string) {
-	args := strings.Fields(text)
-	if len(args) == 3 {
-		command := strings.TrimLeft(args[0], "/")
-		channelData := ChannelData{Command: command, Name: args[1], Topic: args[2]}
-		channelJSON, _ := json.Marshal(channelData)
-
-		//отправка бэку
-		// Отправка данных на бэкенд
-		err := sendJSONToBackend(channelJSON, "add")
-		if err != nil {
-			sendMessage(bot, chatID, "Ошибка в полученных данных"+err.Error())
-		} else {
-			sendMessage(bot, chatID, "Данные успешно зафиксированы")
-		}
-		sendMessage(bot, chatID, "JSON данных канала: "+string(channelJSON))
-	} else {
-		handleUnknownCommand(bot, chatID)
-	}
-}
-
-func handleUnknownCommand(bot *tgbotapi.BotAPI, chatID int64) {
-	reply := "Я не понимаю вашей команды. Воспользуйтесь \n /start \n /view \n /remove <@channelName> <topic> \n /add <@channelName> <topic>\n /help"
-	sendMessage(bot, chatID, reply)
-}
-
-func sendJSONToBackend(jsonData []byte, endpoint string) error {
-	//отправка json
-	return nil
-}
-
-func handleHelp(bot *tgbotapi.BotAPI, chatID int64) {
+func handleHelp(username string) {
 	reply := "\n Мой набор команд включает в себя следующие опции:" + "\n" +
-
 		"/view - для просмотра доступных каналов и связанных с ними тем. \n \n" +
-
 		"/remove <@название канала> <слово> - удаляет указанное слово из списка для поиска в конкретном канале.\n \n " +
-
 		"/add <@название канала> <слово> - добавляет указанное слово в список для поиска в конкретном канале. \n \n " +
-
 		"Эти команды помогут вам управлять списком тем и слов для поиска, чтобы быстро находить нужную информацию в чатах."
-	sendMessage(bot, chatID, reply)
+	sendMessage(username, reply)
+}
+
+func handleView(username string) {
+	topicByChan, err := dataBase.getUserInfo(username)
+	if err != nil {
+		sendMessage(username, err.Error())
+		return
+	}
+	str := strings.Builder{}
+	for ch, topics := range topicByChan {
+		str.WriteString(fmt.Sprintf("%s:\n", ch))
+		for _, topic := range topics {
+			str.WriteString(fmt.Sprintf("  - %s\n", topic))
+		}
+		str.WriteString("\n")
+	}
+	sendMessage(username, str.String())
+}
+
+func handleAdd(username string, msg string) {
+	after, _ := strings.CutPrefix(msg, "/add")
+	concern, err := parseTopic(after)
+	fmt.Println(concern)
+	if err != nil {
+		sendMessage(username, err.Error())
+		return
+	}
+	if err := dataBase.add(username, concern.Channel, concern.Topic); err != nil {
+		sendMessage(username, err.Error())
+		return
+	}
+	sendMessage(username, "Топик добавлен!")
+}
+
+func handleRemove(username, msg string) {
+	after, _ := strings.CutPrefix(msg, "/remove")
+	concern, err := parseTopic(after)
+	fmt.Println(concern)
+	if err != nil {
+		sendMessage(username, err.Error())
+		return
+	}
+	if err := dataBase.removeTopic(username, concern.Channel, concern.Topic); err != nil {
+		sendMessage(username, err.Error())
+		return
+	}
+	sendMessage(username, "Топик удалён!")
+}
+
+func handleUnknownCommand(username string) {
+	reply := "Я не понимаю вашей команды. Воспользуйтесь \n /start \n /view \n /remove <name> <topic> \n /add <name> <topic>"
+	sendMessage(username, reply)
+}
+
+func summarize(text string) string {
+	testRunes := []rune(text)
+
+	length := len(testRunes)
+	if length > summaryLength {
+		length = summaryLength
+	}
+
+	return string(testRunes[:length])
 }
